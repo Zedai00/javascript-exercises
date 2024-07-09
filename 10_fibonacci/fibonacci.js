cf9d9960dfc87d94f8903556a7a2af858679db61
Zedai00 <33319711+Zedai00@users.noreply.github.com>
2024-07-09 17:27:34 +0530

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS"
+  }
+  if (num == 0) {
+    return 0
+  }
+  if (num == 1) {
+    return 1
+  }
+  let a = 0
+  let b = 1
+  let c = 0;
+  for (let i = 0; i <= num - 2; i++) {
+    c = a + b
+    a = b
+    b = c
+  } return c
 };
 
 // Do not edit below this line
