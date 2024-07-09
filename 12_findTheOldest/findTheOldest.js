9be862c0be606e19b4c356c7307e5f73cfe89ce8
Zedai00 <33319711+Zedai00@users.noreply.github.com>
2024-07-09 18:38:57 +0530

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function (people) {
+  let age = 0;
+  let result = people.map((person) => {
+    if (person.yearOfDeath === undefined) {
+      person.yearOfDeath = new Date().getFullYear()
+    }
+    let currAge = person.yearOfDeath - person.yearOfBirth
+    if (currAge > age) {
+      age = currAge
+    }
+    return {
+      name: person.name,
+      currAge
+    }
+  })
+  return result.find((person) => person.currAge === age)
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
