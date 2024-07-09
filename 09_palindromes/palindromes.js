f1f59560ecaaf73148bb5e1a0c8c73502f664f5b
Zedai00 <33319711+Zedai00@users.noreply.github.com>
2024-07-09 16:51:47 +0530

    Complete 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let word = str.toLowerCase()
+  word = word.split("").filter((char) => /^[a-zA-Z0-9]+$/.test(char)).join("")
+  let revWord = word.split("").reverse().join("")
+  if (word === revWord) {
+    return true
+  } else {
+    return false
+  }
 };
 
 // Do not edit below this line
